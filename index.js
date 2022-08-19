6cf9efaf192948fbc67c64990fb9fc40fa96306b
Edwin Alexis <edbwinn@gmail.com>
2022-08-19 17:40:39 -0300

    bug 4 ever

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ connectMongoose
 
 const init = async () => {
 
-    await connectMongoose("mongodb+srv://AlexisB:5pfhqVHP89tqLNgt@blogdenotas.gqmxwvg.mongodb.net/?retryWrites=true&w=majority");
+    await connectMongoose(process.env.MONGODB_URL);
 
-    createServerHttp.listen(3080, () => { console.log(`Servidor funcionando en el puerto ${process.env.PORTDB}`) })
+    createServerHttp.listen(process.env.PORTDB, () => { console.log("Servidor funcionando") })
 }
 
 init()
